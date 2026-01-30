05b18409531746b8fef45fc2e078e93f93ab339d
Anatolii Vorobiov <anatolii.vorobiov@gmail.com>
2026-01-30 22:17:29 +1300

    Add variables for subnet, key, security groups, and instance type
    Fix typo in variable declaration for 'name'.

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "subnet_id" {
+  type = string
+  description = "Subnet ID"
+}
+
+variable "key_name" {
+  type = string
+  description = "Key name"
+}
+
+variable "sg_list"
+{
+  type = list
+  description = "List of Security Groups"
+}
+
+variable "instance_type"
+{
+  type = string
+  description = "Instance type"
+}
+
+varialbe "name"
+{
+  type = string
+  description = "EC2 instance name"
+}
